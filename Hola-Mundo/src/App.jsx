e0138b354f37baf0df90e26c6360bd1f9da28f37
Ernestoc14 <descreed34@hotmail.com>
2023-01-11 23:24:13 -0500

    First Class

---
+++ Hola-Mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export function App() {
+  return(
+    <article>
+      <header>
+        <img src="https://unavatar.io/midudev" alt="El avatar de Midudev" />
+        <div>
+          <strong>Miguel Angel Duran</strong>
+          <span>@midudev</span>
+        </div>
+      </header>
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+    
+  )
+}
