8be7c97bf94891828ef58c5cc032a5bebb01dbd2
Ernestoc14 <descreed34@hotmail.com>
2023-01-12 12:39:48 -0500

    Second Class

--- Hola-Mundo/src/App.jsx
+++ Hola-Mundo/src/App.jsx
@@ -1,17 +1,14 @@
+import React from 'react'
+import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard'
+
 export function App() {
   return(
-    <article>
-      <header>
-        <img src="https://unavatar.io/midudev" alt="El avatar de Midudev" />
-        <div>
-          <strong>Miguel Angel Duran</strong>
-          <span>@midudev</span>
-        </div>
-      </header>
-      <aside>
-        <button>Seguir</button>
-      </aside>
-    </article>
-    
+    <section className='App'>
+      <TwitterFollowCard isFollowing={true} userName="midudev" name="Miguel Angel Duran"/>
+      <TwitterFollowCard isFollowing={false} userName="ernie" name="Ernie Codes"/>
+      <TwitterFollowCard isFollowing={true} userName="elonmusk" name="Elon Musk"/>
+      <TwitterFollowCard isFollowing={false} userName="dualipa" name="Dua Lipa"/>
+    </section>
   )
 }
---
+++ Hola-Mundo/src/TwitterFollowCard.jsx
@@ -0,0 +1,21 @@
+export function TwitterFollowCard({userName, name, isFollowing}) {
+    return (
+    <article className="tw-followCard">
+    <header className="tw-followCard-header">
+        <img
+        className="tw-followCard-avatar"
+        src={`https://unavatar.io/${userName}`}
+        alt="El avatar de Midudev"
+        />
+        <div className="tw-followCard-info">
+            <strong>{name}</strong> 
+            <span className="tw-followCard-username">@{userName}</span>
+        </div>
+    </header>
+
+        <aside>
+            <button className="tw-followCard-button">Seguir</button>
+        </aside>
+    </article>
+    );
+}
