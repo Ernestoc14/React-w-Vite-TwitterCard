ee521fba499d1e2ee7861623773d5fe347ee85e4
Ernestoc14 <descreed34@hotmail.com>
2023-02-26 23:03:01 -0500

    ESLint Update

--- 02-Tic-Tac-Toe/src/App.jsx
+++ 02-Tic-Tac-Toe/src/App.jsx
@@ -1,40 +1,31 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const TURNS = {
-  x: 'x',
-  o: 'o'
-}
+  x: "x",
+  o: "o",
+};
 
 const Square = ({ children, updateBoard, index }) => {
-  return(
-    <div className="square">
-      {children}
-    </div>
-  )
-}
+  return <div className="square">{children}</div>;
+};
 
 function App() {
-  const [board, setBoard] = useState(Array(9).fill(null))  
-  
+  const [board, setBoard] = useState(Array(9).fill(null));
+
   return (
-    <main className='board'>
-      <h1>React + Vite</h1>
+    <main className="board">
+      <h1>Tic Tac Toe</h1>
       <section className="game">
-        {
-          board.map((_, index) =>{
-            return (
-              <Square 
-                key={index}
-                index={index}
-              >
-                {index}
-              </Square>
-            ) 
-          })
-        }
+        {board.map((_, index) => {
+          return (
+            <Square key={index} index={index}>
+              {index}
+            </Square>
+          );
+        })}
       </section>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
